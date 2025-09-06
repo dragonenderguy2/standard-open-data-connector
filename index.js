99cb7b73a1754aea42fe8df944129e24f194a288
dragonenderguy2 <shaihalapre@hotmail.com>
2025-09-06 13:56:48 -0500

    Increase timeout for axios requests from 5000ms to 10000ms.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function fetchData(url) {
     try {
-        const response = await axios.get(url, { timeout: 5000 });
+        const response = await axios.get(url, { timeout: 10000 });
         return response.data;
     } catch (error) {
         console.error(`Error fetching data from ${url}: ${error}`);
